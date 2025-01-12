6a603ae695cde4d3cf41641b66ea188b068c2899
SuperKXT <superkxt@outlook.com>
2025-01-12 18:11:53 +0500

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -356,7 +356,7 @@ const config = {
 			},
 		},
 		{
-			files: ["app/routes.ts", "app/root.tsx", "**/*.route.tsx"],
+			files: ["./*", "app/routes.ts", "app/root.tsx", "**/*.route.tsx"],
 			rules: {
 				"import/no-default-export": "off",
 			},
