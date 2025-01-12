5cbe0181d8dc971501900efb0e72893d7167a80f
SuperKXT <superkxt@outlook.com>
2025-01-12 12:17:43 +0500

    Add theme and custom font

--- app/root.tsx
+++ app/root.tsx
@@ -2,26 +2,30 @@ import "@mantine/core/styles.css";
 
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import { theme } from "./theme/theme";
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-        <ColorSchemeScript />
-      </head>
-      <body>
-        <MantineProvider>{children}</MantineProvider>
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+				<Meta />
+				<Links />
+				<ColorSchemeScript />
+			</head>
+			<body>
+				<MantineProvider theme={theme}>{children}</MantineProvider>
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
 }
 
 export default function App() {
-  return <Outlet />;
+	return <Outlet />;
 }
---
+++ app/theme/theme.ts
@@ -0,0 +1,7 @@
+import { createTheme } from "@mantine/core";
+
+import "./theme.css";
+
+export const theme = createTheme({
+	fontFamily: "Lexend, sans-serif",
+});
