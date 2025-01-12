af6c1304ca7e25d7f7e96aa1e31fac2bb81f9249
mbm_1607 <muhammadkhan1607@gmail.com>
2025-01-12 11:37:28 +0500

    :art: Format files using prettier

--- app/root.tsx
+++ app/root.tsx
@@ -4,24 +4,27 @@ import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-        <ColorSchemeScript />
-      </head>
-      <body>
-        <MantineProvider>{children}</MantineProvider>
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+				<Meta />
+				<Links />
+				<ColorSchemeScript />
+			</head>
+			<body>
+				<MantineProvider>{children}</MantineProvider>
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
 }
 
 export default function App() {
-  return <Outlet />;
+	return <Outlet />;
 }
