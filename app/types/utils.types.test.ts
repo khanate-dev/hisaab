7517b23755250a993148963a052692ba51f1d175
SuperKXT <superkxt@outlook.com>
2025-03-29 04:21:21 +0500

    Add util types

---
+++ app/types/utils.types.test.ts
@@ -0,0 +1,513 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-redundant-type-constituents */
+import { assertType, test } from "vitest";
+
+import type { Utils } from "./utils.types.js";
+
+type TTrueTuple<T extends true[]> = T[number] extends true ? true : false;
+type TFalseTuple<T extends false[]> = T[number] extends false ? true : false;
+
+test("test prettify type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<
+				Utils.prettify<{ x: 1 } & { y: { a: 2 } | { b: 2 } }>,
+				{ x: 1; y: { a: 2 } | { b: 2 } }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test equal type util", () => {
+	interface TInt1 {
+		x: 1;
+	}
+	interface TInt2 extends TInt1 {
+		y: 2;
+	}
+	type TTrueTests = TTrueTuple<
+		[
+			Utils.equal<1, 1>,
+			Utils.equal<1 | 2 | 3, 1 | 2 | 3>,
+			Utils.equal<[1, 2, 3], [1, 2, 3]>,
+			Utils.equal<[2] | [1, 2, 3], [1, 2, 3] | [2]>,
+			Utils.equal<{ x: 1 } | { y: 2 }, { y: 2 } | { x: 1 }>,
+			Utils.equal<{ x: 1 } & { y: 2 }, { x: 1; y: 2 }>,
+			Utils.equal<TInt2, { x: 1; y: 2 }>,
+			Utils.equal<
+				TInt2 & { z: { a: 3; b: 4 } },
+				{ x: 1; y: 2; z: { a: 3 } & { b: 4 } }
+			>,
+			Utils.equal<TInt2[] | TInt2, { x: 1; y: 2 }[] | { x: 1; y: 2 }>,
+			Utils.equal<Record<string, unknown>, Record<string, unknown>>,
+		]
+	>;
+	type TFalseTests = TFalseTuple<
+		[
+			Utils.equal<any, unknown>,
+
+			Utils.equal<any, 1>,
+
+			Utils.equal<any, never>,
+
+			Utils.equal<any, {}>,
+			Utils.equal<unknown, never>,
+			Utils.equal<unknown, {}>,
+			Utils.equal<1, 1 | 2 | 3>,
+			Utils.equal<[1], [1 | 2 | 3]>,
+			Utils.equal<{ foo: 1 }, { foo: 2 }>,
+			Utils.equal<{ x: 1 }, Record<string, unknown>>,
+			Utils.equal<object, Record<string, unknown>>,
+		]
+	>;
+	assertType<[TTrueTests, TFalseTests]>([true, true]);
+});
+
+test("test satisfies type util", () => {
+	type _TTests = [
+		Utils.satisfies<1, 1>,
+		Utils.satisfies<1 | 2 | 3, number>,
+		Utils.satisfies<[1, 2, 3], unknown[]>,
+		Utils.satisfies<{ x: 1; y: 2; z: 3 }, { x: 1; y: 2 }>,
+		Utils.satisfies<(val: string) => void, (...args: any[]) => unknown>,
+		// @ts-expect-error does not satisfies
+		Utils.satisfies<1 | 2, string>,
+		// @ts-expect-error does not satisfies
+		Utils.satisfies<{ x: 1 }, { x: 1; y: 2 }>,
+		// @ts-expect-error does not satisfies
+		Utils.satisfies<[1, 2, "3"], number[]>,
+	];
+});
+
+test("test dropFirst type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.dropFirst<[1, 2, 3]>, [2, 3]>,
+			Utils.equal<Utils.dropFirst<[]>, []>,
+			Utils.equal<Utils.dropFirst<readonly string[]>, readonly string[]>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test tuple type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.tuple<5>, [1, 1, 1, 1, 1]>,
+			Utils.equal<Utils.tuple<1, 1 | 2>, [1 | 2]>,
+			Utils.equal<Utils.tuple<0, string>, []>,
+			Utils.equal<Utils.tuple<1, string | { foo: 1 }>, [string | { foo: 1 }]>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test repeatString type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.repeatString<"1", 5>, "11111">,
+			Utils.equal<Utils.repeatString<"", 5>, "">,
+			Utils.equal<Utils.repeatString<"", 0>, "">,
+			Utils.equal<Utils.repeatString<"foo" | "bar", 2>, "foofoo" | "barbar">, // cSpell: disable-line,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test trim type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.trim<"  1   ">, "1">,
+			Utils.equal<Utils.trim<"">, "">,
+			Utils.equal<Utils.trim<"  foo">, "foo">,
+			Utils.equal<Utils.trim<"foo  " | " bar" | "baz">, "foo" | "bar" | "baz">,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test distributedArray type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.distributedArray<1 | 2 | 3>, 1[] | 2[] | 3[]>,
+			Utils.equal<Utils.distributedArray<{ foo: 1 | 2 }>, { foo: 1 | 2 }[]>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test assertFunction type util", () => {
+	const func1: Utils.assertFunction<string> = () => {
+		return undefined;
+	};
+	const val1 = {} as unknown;
+	assertType<unknown>(val1);
+	func1(val1);
+	assertType<string>(val1);
+
+	const func2: Utils.assertFunction<{ foo: 1 }> = () => {
+		return undefined;
+	};
+	const val2 = {} as unknown;
+	assertType<unknown>(val2);
+	func2(val2);
+	assertType<{ foo: 1 }>(val2);
+});
+
+test("test keysOfType type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.keysOfType<{ foo: 1; bar: 2 }, 1 | 2>, "foo" | "bar">,
+			Utils.equal<Utils.keysOfType<{ foo?: 1 }, 1 | undefined>, "foo">,
+			Utils.equal<Utils.keysOfType<{ foo?: 1; bar: 2 }, 1>, never>,
+			Utils.equal<Utils.keysOfType<{ foo: 1; bar: 2 }, 3>, never>,
+			Utils.equal<
+				Utils.keysOfType<
+					{ foo: { baz: number }; bar: { baz: number | string } },
+					{ baz: number }
+				>,
+				"foo"
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test allUnionKeys type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.allUnionKeys<{ x: 1 } | { y: 2 }>, "x" | "y">,
+			Utils.equal<Utils.allUnionKeys<{ x: 1 }>, "x">,
+			Utils.equal<Utils.allUnionKeys<never>, never>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test includeUnionKeys type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<
+				Utils.includeUnionKeys<{ x: 1 } | { y: 2 }>,
+				{ x: 1; y?: never } | { y: 2; x?: never }
+			>,
+			Utils.equal<Utils.includeUnionKeys<{ x: 1 }>, { x: 1 }>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test noUndefinedKeys type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<
+				Utils.noUndefinedKeys<{ x: 1; y: undefined } | { y: 2; x: undefined }>,
+				{ x: 1; y: never } | { y: 2; x: never }
+			>,
+			Utils.equal<
+				Utils.noUndefinedKeys<{ x: 1; y: undefined; z: 2 }>,
+				{ x: 1; y: never; z: 2 }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test strictly type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<
+				Utils.strictly<{ foo: 1; bar: 2 }, { foo: 1 }>,
+				{ foo: 1 } & { foo: 1; bar: never }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test allOrNone type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<
+				Utils.allOrNone<{ foo: 1; bar: 2 } | { baz: 3 }>,
+				| { foo: 1; bar: 2 }
+				| { foo?: never; bar?: never }
+				| { baz: 3 }
+				| { baz?: never }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test allOrNullable type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<
+				Utils.allOrNull<{ foo: 1; bar: 2 } | { baz?: 3 }>,
+				| { foo: 1; bar: 2 }
+				| { foo: null; bar: null }
+				| { baz?: 3 }
+				| { baz?: null }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test makeUndefinedOptional type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.makeUndefinedOptional<{ x: 1 }>, { x: 1 }>,
+			Utils.equal<
+				Utils.makeUndefinedOptional<{ x: 1; y: undefined; z?: 1 }>,
+				{ x: 1; y?: undefined; z?: 1 }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test removeIndexSignature type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<
+				Utils.removeIndexSignature<{ x: 1; [x: PropertyKey]: number }>,
+				{ x: 1 }
+			>,
+			Utils.equal<Utils.removeIndexSignature<{ x: 1 }>, { x: 1 }>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test distributiveOmit type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<
+				Utils.distributiveOmit<{ x: 1; y: 2 } | { z: 3 }, "y">,
+				{ x: 1 } | { z: 3 }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test stringToUnion type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.stringToUnion<"1">, "1">,
+			Utils.equal<Utils.stringToUnion<"hello">, "h" | "e" | "l" | "o">,
+			Utils.equal<
+				Utils.stringToUnion<"coronavirus">,
+				"c" | "o" | "r" | "n" | "a" | "v" | "i" | "u" | "s"
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test unionToIntersection type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<
+				Utils.unionToIntersection<"foo" | 42 | true>,
+				"foo" & 42 & true
+			>,
+			Utils.equal<
+				Utils.unionToIntersection<(() => "foo") | ((i: 42) => true)>,
+				(() => "foo") & ((i: 42) => true)
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test takeOneFromUnion type util", () => {
+	type TUnion = "foo" | 42 | true;
+	type TMember = Utils.takeOneFromUnion<"foo" | 42 | true>;
+
+	type TTests = TTrueTuple<
+		[
+			TMember extends TUnion ? true : false,
+			Utils.isUnion<TMember> extends true ? false : true,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test unionToSingleTuple type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<
+				Utils.unionToSingleTuple<"foo" | 42 | true>,
+				["foo" | 42 | true, "foo" | 42 | true, "foo" | 42 | true]
+			>,
+			Utils.equal<
+				Utils.unionToSingleTuple<(() => "foo") | ((i: 42) => true)>,
+				[(() => "foo") | ((i: 42) => true), (() => "foo") | ((i: 42) => true)]
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test deepMerge type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<
+				Utils.deepMerge<
+					{ a: { b: 1; c: 2 }; c: 2 },
+					{ a: { b: 11; d: 33 }; c: 1 }
+				>,
+				{ a: { b: 11; c: 2; d: 33 }; c: 1 }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test unionToTuples type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.unionToTuples<never>, []>,
+			Utils.equal<
+				Utils.unionToTuples<1 | 2 | 3>,
+				[1, 2, 3] | [1, 3, 2] | [2, 1, 3] | [2, 3, 1] | [3, 1, 2] | [3, 2, 1]
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test nonNullableKeys type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.nonNullableKeys<{ foo: 1 | null }>, { foo: 1 }>,
+			Utils.equal<
+				Utils.nonNullableKeys<
+					{ foo: 1 | null | undefined; bar: 2 | null },
+					"foo"
+				>,
+				{ foo: 1; bar: 2 | null }
+			>,
+			Utils.equal<
+				Utils.nonNullableKeys<{
+					foo: 1 | null;
+					bar?: 2 | 3 | undefined;
+					baz: null;
+				}>,
+				{ foo: 1; bar?: 2 | 3; baz: never }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test nullableKeys type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.nullableKeys<{ foo: 1 }>, { foo: 1 | null }>,
+			Utils.equal<
+				Utils.nullableKeys<{ foo: 1; bar: 2 }, "foo">,
+				{ foo: 1 | null; bar: 2 }
+			>,
+			Utils.equal<
+				Utils.nullableKeys<{ foo: 1; bar?: 2 | 3; baz: 4 | null }>,
+				{ foo: 1 | null; bar?: 2 | 3 | null; baz: 4 | null }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test optionalKeys type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.optionalKeys<{ foo: 1 }>, { foo?: 1 }>,
+			Utils.equal<
+				Utils.optionalKeys<{ foo: 1; bar: 2 }, "foo">,
+				{ foo?: 1; bar: 2 }
+			>,
+			Utils.equal<
+				Utils.optionalKeys<{ foo: 1; bar?: 2 | 3; baz: 4 | null }>,
+				{ foo?: 1; bar?: 2 | 3; baz?: 4 | null }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test requiredKeys type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.requiredKeys<{ foo?: 1 }>, { foo: 1 }>,
+			Utils.equal<
+				Utils.requiredKeys<{ foo?: 1; bar: 2 }, "foo">,
+				{ foo: 1; bar: 2 }
+			>,
+			Utils.equal<
+				Utils.requiredKeys<{ foo?: 1; bar?: 2 | 3; baz: 4 | null | undefined }>,
+				{ foo: 1; bar: 2 | 3; baz: 4 | null | undefined }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test extractClassProps type util", () => {
+	interface TTest {
+		foo: 1;
+		bar: 2;
+		func: () => string;
+		opt?: number;
+		func2: () => string;
+	}
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<
+				Utils.extractClassProps<TTest>,
+				{ foo: 1; bar: 2; opt?: number }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test numberEnumFromTuple type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<
+				Utils.numberEnumFromTuple<["foo", "bar", "baz"]>,
+				{ foo: 0; bar: 1; baz: 2 }
+			>,
+			Utils.equal<
+				Utils.numberEnumFromTuple<["foo", "bar"] | ["baz"]>,
+				{ foo: 0; bar: 1 } | { baz: 0 }
+			>,
+		]
+	>;
+	assertType<TTests>(true);
+});
+
+test("test isUnion type util", () => {
+	type TTests = TTrueTuple<
+		[
+			Utils.equal<Utils.isUnion<string>, false>,
+			Utils.equal<Utils.isUnion<boolean>, true>,
+			Utils.equal<Utils.isUnion<string | number>, true>,
+			Utils.equal<Utils.isUnion<"a" | "b" | "c" | "d">, true>,
+			Utils.equal<Utils.isUnion<undefined | null | void | "">, true>,
+			Utils.equal<Utils.isUnion<{ a: string } | { a: number }>, true>,
+			Utils.equal<Utils.isUnion<{ a: string | number }>, false>,
+			Utils.equal<Utils.isUnion<[string | number]>, false>,
+			Utils.equal<Utils.isUnion<string | never>, false>,
+			Utils.equal<Utils.isUnion<string | unknown>, false>,
+			Utils.equal<Utils.isUnion<string | any>, false>,
+			Utils.equal<Utils.isUnion<string | "a">, false>,
+			Utils.equal<Utils.isUnion<never>, false>,
+		]
+	>;
+	assertType<TTests>(true);
+});
---
+++ app/types/utils.types.ts
@@ -0,0 +1,233 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+type _repeatString<
+	S extends string,
+	N extends number,
+	T extends 1[] = [],
+> = T["length"] extends N ? "" : `${S}${_repeatString<S, N, [...T, 1]>}`;
+
+type _tuple<N extends number, T, R extends readonly T[]> = R["length"] extends N
+	? R
+	: _tuple<N, T, [T, ...R]>;
+
+type _equal<T, U> =
+	// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
+	(<G>() => G extends T ? 1 : 2) extends <G>() => G extends U ? 1 : 2
+		? true
+		: false;
+
+type _recursivePrettify<T> = {
+	[k in keyof T]: T[k] extends object ? _recursivePrettify<T[k]> : T[k];
+} & {};
+
+type _unionToSingleTuple<
+	union,
+	remaining extends union = union,
+	curr extends remaining = remaining,
+> = [remaining] extends [never]
+	? []
+	: curr extends curr
+		? [union, ..._unionToSingleTuple<union, Exclude<remaining, curr>>]
+		: never;
+
+export declare namespace Utils {
+	/** types that resolve as falsy  */
+	type falsy = false | "" | 0 | null | undefined;
+
+	/** type helper to prettify complex object types */
+	type prettify<T> = { [k in keyof T]: T[k] } & {};
+
+	/** checks if the two given types are the same */
+	type equal<T, U> = [T, U] extends [object, object]
+		? _equal<_recursivePrettify<T>, _recursivePrettify<U>>
+		: _equal<T, U>;
+
+	/** checks if the first type satisfies the second */
+	type satisfies<T extends U, U> = T;
+
+	type dropFirst<T extends readonly unknown[]> = number extends T["length"]
+		? T
+		: T extends readonly [unknown, ...infer U]
+			? U
+			: [];
+
+	/** global type helper to repeat a type `N` times in a tuple */
+	type tuple<N extends number, T = 1> = N extends N
+		? number extends N
+			? T[]
+			: _tuple<N, T, []>
+		: never;
+
+	/** global type helper to repeat a string `N` times in a string literal type */
+	type repeatString<S extends string, N extends number> = S extends S
+		? _repeatString<S, N>
+		: never;
+
+	/** trim the empty spaces from the start and end of the string */
+	type trim<str extends string> = str extends
+		| ` ${infer trimmed}`
+		| `${infer trimmed} `
+		? trim<trimmed>
+		: str;
+
+	/** global type helper to create a union array type from a union type */
+	type distributedArray<T> = T extends infer I ? I[] : never;
+
+	/** global type helper to be able to use arrow functions for assertions */
+	type assertFunction<Type> = (value: unknown) => asserts value is Type;
+
+	/** return only the keys of the object whose value is assignable to the given type */
+	type keysOfType<T, Match> = {
+		[k in keyof T]-?: T[k] extends Match ? k : never;
+	}[keyof T];
+
+	/** Return a union of keys from all objects in the union */
+	type allUnionKeys<T> = T extends infer U ? keyof U : never;
+
+	/** returns a uniformed union of objects by adding missing keys in each union */
+	type includeUnionKeys<T extends Record<string, unknown>, U = T> = U extends U
+		? prettify<
+				{
+					[K in keyof U]: U[K];
+				} & Partial<Record<Exclude<allUnionKeys<T>, keyof U>, never>>
+			>
+		: never;
+
+	/** Disallow explicitly undefined value for object keys. Used when generic param is constrained to `Partial<ObjType>` */
+	type noUndefinedKeys<T extends object> = {
+		[k in keyof T]: T[k] extends undefined ? never : T[k];
+	};
+
+	/** matches a type to another exactly. Used with generic functions to make sure the object type matches exactly */
+	type strictly<T, Shape> = Shape & {
+		[k in keyof T]: k extends keyof Shape ? Shape[k] : never;
+	};
+
+	/** creates a union of the given object or an object where all the keys of the object are undefined */
+	type allOrNone<T extends object> = T | { [k in keyof T]?: never };
+
+	/** creates a union of the given object or an object where all the keys of the object are null */
+	type allOrNull<T extends object> = T | { [k in keyof T]: null };
+
+	/** make keys that can be undefined optional in the object */
+	type makeUndefinedOptional<T extends object> = prettify<
+		{
+			[k in keyof T as undefined extends T[k] ? k : never]?: T[k];
+		} & {
+			[k in keyof T as undefined extends T[k] ? never : k]: T[k];
+		}
+	>;
+
+	/** remove index signatures from an object type */
+	type removeIndexSignature<T extends object> = {
+		[k in keyof T as string extends k
+			? never
+			: number extends k
+				? never
+				: symbol extends k
+					? never
+					: k]: T[k];
+	};
+
+	/** omit utility that distributes over the union */
+	type distributiveOmit<T, K extends PropertyKey> = T extends T
+		? Omit<T, K>
+		: never;
+
+	/** takes a string literal as input and returns the union of all the characters */
+	type stringToUnion<T extends string> = T extends `${infer U}${infer V}`
+		? U | stringToUnion<V>
+		: never;
+
+	/** takes a union of types and converts it into intersection of the types */
+	type unionToIntersection<T> = (
+		T extends unknown ? (x: T) => unknown : never
+	) extends (x: infer U) => unknown
+		? U
+		: never;
+
+	/** takes one type from a union of types */
+	type takeOneFromUnion<T> =
+		unionToIntersection<T extends T ? (x: T) => 0 : never> extends (
+			x: infer U,
+		) => 0
+			? U
+			: never;
+
+	/** converts a union to a single tuple where each member is of union type */
+	type unionToSingleTuple<union> = _unionToSingleTuple<union>;
+
+	/** merge two objects together. the second object has priority */
+	type deepMerge<T extends object, U extends object> = prettify<{
+		[k in keyof T | keyof U]: k extends keyof U
+			? k extends keyof T
+				? T[k] extends object
+					? U[k] extends object
+						? deepMerge<T[k], U[k]>
+						: U[k]
+					: U[k]
+				: U[k]
+			: k extends keyof T
+				? T[k]
+				: never;
+	}>;
+
+	/** convert a given union to a union of permutation of tuples */
+	type unionToTuples<T, U = T> = [T] extends [never]
+		? []
+		: U extends U
+			? [U, ...unionToTuples<Exclude<T, U>>]
+			: [];
+
+	/** make the given keys (all if second arg is omitted) of an object non-nullable (null and undefined are removed) */
+	type nonNullableKeys<obj, keys extends keyof obj = keyof obj> = {
+		[k in keyof obj]: k extends keys ? NonNullable<obj[k]> : obj[k];
+	};
+
+	/** make the given keys (all if second arg is omitted) of an object nullable */
+	type nullableKeys<obj, keys extends keyof obj = keyof obj> = {
+		[k in keyof obj]: k extends keys ? obj[k] | null : obj[k];
+	};
+
+	/** make the given keys (all if second arg is omitted) of an object optional */
+	type optionalKeys<obj, keys extends keyof obj = keyof obj> = prettify<
+		{
+			[k in keyof obj as k extends keys ? never : k]: obj[k];
+		} & {
+			[k in keyof obj as k extends keys ? k : never]?: obj[k];
+		}
+	>;
+
+	/** make the given keys (all if second arg is omitted) of an object required */
+	type requiredKeys<obj, keys extends keyof obj = keyof obj> = prettify<
+		{
+			[k in keyof obj as k extends keys ? never : k]: obj[k];
+		} & {
+			[k in keyof obj as k extends keys ? k : never]-?: obj[k];
+		}
+	>;
+
+	/** extract the properties of a class */
+	type extractClassProps<T> = {
+		[k in keyof T as T[k] extends (...args: any[]) => unknown
+			? never
+			: k]: T[k];
+	};
+
+	type numberEnumFromTuple<Tuple extends readonly string[]> =
+		Tuple extends Tuple
+			? Utils.prettify<{
+					-readonly [k in keyof Omit<Tuple, keyof unknown[]> as Tuple[k] &
+						PropertyKey]: `${k & string}` extends `${infer num extends number}`
+						? num
+						: never;
+				}>
+			: never;
+
+	/** checks if the given type is a union */
+	type isUnion<T, U = T> = (
+		T extends T ? (U extends T ? true : false) : false
+	) extends true
+		? false
+		: true;
+}
