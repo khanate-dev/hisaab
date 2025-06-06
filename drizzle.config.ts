3cd6f4b95e61677cbb6f8260b5c3f28725969aee
SuperKXT <superkxt@outlook.com>
2025-06-06 18:58:33 +0500

    Update `drizzle.config.ts`

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
+import { defineConfig } from "drizzle-kit";
+
 import { getTursoCredentials } from "~/db/turso.config";
 
-import type { Config } from "drizzle-kit";
-
-export default {
-	schema: "./db/db.schema.ts",
-	out: "./migrations",
+export default defineConfig({
+	schema: "./app/db/db.schema.ts",
+	out: "./drizzle",
 	dialect: "turso",
 	dbCredentials: getTursoCredentials(),
-} satisfies Config;
+});
